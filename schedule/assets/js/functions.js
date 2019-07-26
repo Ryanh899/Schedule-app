4be3bcf4cbeff4a225635e27ef607e90d131d7c1
Ryan Hazzard <rhazzardphoto@gmail.com>
2019-07-26 10:59:56 -0700

    uses prompt and appends to schedule

--- schedule/assets/js/functions.js
+++ schedule/assets/js/functions.js
@@ -4,13 +4,28 @@ function scheduling () {
         newDay = moment().add(i, 'd');
         var append = newDay.toString().slice(0,10); 
     if (i <= 6) {
-        $('#week-1').append(`<td class="pb-5" >${append} <button id="${append}"> Add Shift </button> </td>`)
+        $('#week-1').append(`<td class="pb-5" id="${append}" >${append} <button class="btn btn-primary shiftButton" id="${append}"> Add Shift </button> </td>`)
     } else if (i <= 13) {
-        $('#week-2').append(`<td class="pb-5" >${append} <button id="${append}"> Add Shift </button> </td>`)
+        $('#week-2').append(`<td class="pb-5" id="${append}" >${append} <button class="btn btn-primary shiftButton" id="${append}"> Add Shift </button> </td>`)
     } else if (i <= 20) {
-        $('#week-3').append(`<td class="pb-5" >${append} <button id="${append}"> Add Shift </button> </td>`)
+        $('#week-3').append(`<td class="pb-5" id="${append}" >${append} <button class="btn btn-primary shiftButton" id="${append}"> Add Shift </button> </td>`)
     } else if (i <= 27) {
-        $('#week-4').append(`<td class="pb-5" >${append} <button id="${append}"> Add Shift </button> </td>`)
+        $('#week-4').append(`<td class="pb-5" id="${append}">${append} <button class="btn btn-primary shiftButton" id="${append}"> Add Shift </button> </td>`)
     } 
     }
+}
+
+//constructor for schedule object 
+function employeeByDay (date, employee, hours) {
+    this.date = date;
+    this.employee = employee; 
+    this.hours = hours;
+}
+
+//function for what happens on 'add shift' button click
+function addShift () {
+    $('.shiftButton').on('click', function () {
+        var shift = new employeeByDay($(this).attr('id'), prompt('Pick an employee'), prompt('Input hours') );
+        $(this).parent().append(`<p id="${$(this).employee}"> ${shift.employee}: ${shift.hours.toString()}</p>`);
+    })
 }
